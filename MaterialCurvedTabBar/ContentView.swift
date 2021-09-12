5b88d67045ddd21ff80da036d8d8510701ab3b52
jyrnan <yoeking@gmail.com>
2021-09-12 09:48:22 +0800

    Initial Commit

---
+++ MaterialCurvedTabBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MaterialCurvedTabBar
+//
+//  Created by Yong Jin on 2021/9/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MaterialCurvedTabBar/MaterialCurvedTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaterialCurvedTabBarApp.swift
+//  MaterialCurvedTabBar
+//
+//  Created by Yong Jin on 2021/9/12.
+//
+
+import SwiftUI
+
+@main
+struct MaterialCurvedTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
